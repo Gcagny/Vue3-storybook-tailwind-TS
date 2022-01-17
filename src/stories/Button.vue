4ebe76682a3c9018dd16c334521bd3a76de3a029
Guilhem <cagny.guilhem@gmail.com>
2022-01-17 15:53:21 +0100

    ON continue tranquillou

--- src/stories/Button.vue
+++ src/stories/Button.vue
@@ -1,38 +1,62 @@
 <template>
-  <div class="flex flex-col-reverse">
-      <button type="button" :class="classes" @click="onClick" :style="style">{{ label }}</button>
-      <button type="button" :class="classes" @click="onClick" :style="style">{{ label }}</button>
-
-  </div>
+  <button type="button" :class="[classes, buttonType, buttonSize]" @click="onClick">
+    <i :class="'fas ' + icon" v-if="icon && icon.length > 0" />
+    <span>{{ label }}</span>
+  </button>
 </template>
 
 <script setup lang="ts">
-  import './button.css';
   import { computed } from 'vue';
   interface Props {
-    label: {
-      type: string,
-      required: true
-    },
-    primary: boolean,
-    size: string,
-    backgroundColor: string
+    label: string,
+    outlined?: boolean,
+    text?: boolean,
+    small?: string,
+    large?: boolean,
+    icon?: string
   }
-  const props = withDefaults(defineProps<Props>(), { primary: false });
+  const props = defineProps<Props>();
   const emit = defineEmits({
     click: () => true
   });
+  const buttonType = computed(() => {
+    if (props.outlined) return 'outlined';
+    else if (props.text) return 'text';
+    else return 'filled';
+  });
+  const buttonSize = computed(() => {
+    if (props.small) return 'small';
+    else if (props.large) return 'large';
+    else return 'medium';
+  })
   const classes = computed (() => ({
-    'storybook-button': true,
-    'storybook-button--primary': props.primary,
-    'storybook-button--secondary': !props.primary,
-    [`storybook-button--${props.size || 'medium'}`]: true
-  }));
-  const style = computed(() => ({
-    'backgroundColor': props.backgroundColor
+    'ariane-button': true,
   }));
 
   function onClick () {
     emit('click');
   }
 </script>
+
+<style>
+  .ariane-button {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+  }
+  .padding-button {
+    padding: 8px 30px;
+    border-radius: 3px;
+  }
+  .filled {
+    background-color: aquamarine;
+    color: white;
+  }
+  .outlined {
+    background-color: white;
+    color: aquamarine;
+  }
+  .filled, .outlined  {
+    @apply padding-button;
+  }
+</style>
